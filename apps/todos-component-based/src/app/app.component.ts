f439d9ae6973132fcd42714920772966da4975c7
on3dd <onedeadwave.work@gmail.com>
2024-08-04 18:48:10 +1000

    refactor(todos-component-based): moved todos template & logic in dedicated component

--- apps/todos-component-based/src/app/app.component.ts
+++ apps/todos-component-based/src/app/app.component.ts
@@ -1,49 +1,14 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
-import { ToastsModule, ToastsService } from '@angular-todos/toasts';
-import { Todo, TodosApiService, TodosModule } from '@angular-todos/todos';
-import { tap } from 'rxjs';
+import { Component } from '@angular/core';
+import { ToastsModule } from '@angular-todos/toasts';
+
+import { TodosComponent } from './todos/todos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ToastsModule, TodosModule],
+  imports: [ToastsModule, TodosComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'todos-component-based';
-
-  private readonly toastsService = inject(ToastsService);
-
-  private readonly todosService = inject(TodosApiService);
-
-  todos = signal<Todo[]>([]);
-
-  todosCount = computed(() => this.todos().length);
-
-  ngOnInit() {
-    this.todosService.getTodos().subscribe((todos) => this.todos.set(todos));
-  }
-
-  onTodoAdded(title: string) {
-    this.todosService
-      .addTodo(title)
-      .pipe(tap(() => this.toastsService.show({ text: 'Todo has been successfully added.', type: 'success' })))
-      .subscribe((newTodo) => this.todos.update((todos) => [...todos, newTodo]));
-  }
-
-  onTodoToggled(todo: Todo) {
-    this.todosService
-      .toggleTodo(todo.id, !todo.completed)
-      .pipe(tap(() => this.toastsService.show({ text: 'Todo has been successfully updated.', type: 'success' })))
-      .subscribe((updatedTodo) =>
-        this.todos.update((todos) => todos.map((item) => (item.id === todo.id ? { ...todo, ...updatedTodo } : item))),
-      );
-  }
-
-  onTodoDeleted(todo: Todo) {
-    this.todosService
-      .deleteTodo(todo.id)
-      .pipe(tap(() => this.toastsService.show({ text: 'Todo has been successfully deleted.', type: 'success' })))
-      .subscribe(() => this.todos.update((todos) => todos.filter((item) => item.id !== todo.id)));
-  }
 }
---
+++ apps/todos-component-based/src/app/todos/todos.component.ts
@@ -0,0 +1,49 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, inject, OnInit, signal } from '@angular/core';
+import { ToastsService } from '@angular-todos/toasts';
+import { Todo, TodosApiService, TodosModule } from '@angular-todos/todos';
+import { tap } from 'rxjs';
+
+@Component({
+  selector: 'app-todos',
+  standalone: true,
+  imports: [CommonModule, TodosModule],
+  templateUrl: './todos.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TodosComponent implements OnInit {
+  private readonly toastsService = inject(ToastsService);
+
+  private readonly todosService = inject(TodosApiService);
+
+  todos = signal<Todo[]>([]);
+
+  todosCount = computed(() => this.todos().length);
+
+  ngOnInit() {
+    this.todosService.getTodos().subscribe((todos) => this.todos.set(todos));
+  }
+
+  onTodoAdded(title: string) {
+    this.todosService
+      .addTodo(title)
+      .pipe(tap(() => this.toastsService.show({ text: 'Todo has been successfully added.', type: 'success' })))
+      .subscribe((newTodo) => this.todos.update((todos) => [...todos, newTodo]));
+  }
+
+  onTodoToggled(todo: Todo) {
+    this.todosService
+      .toggleTodo(todo.id, !todo.completed)
+      .pipe(tap(() => this.toastsService.show({ text: 'Todo has been successfully updated.', type: 'success' })))
+      .subscribe((updatedTodo) =>
+        this.todos.update((todos) => todos.map((item) => (item.id === todo.id ? { ...todo, ...updatedTodo } : item))),
+      );
+  }
+
+  onTodoDeleted(todo: Todo) {
+    this.todosService
+      .deleteTodo(todo.id)
+      .pipe(tap(() => this.toastsService.show({ text: 'Todo has been successfully deleted.', type: 'success' })))
+      .subscribe(() => this.todos.update((todos) => todos.filter((item) => item.id !== todo.id)));
+  }
+}
